14c146dd50bbe72822239f0f241cc5a5aad5c380
Chris <chris.sun5567@email.com>
2021-08-15 05:35:22 +0000

    add SASS, top toggle button, and bottom toggle button

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,6 +5,30 @@
  */
 
 $(document).ready(() => {
+  $(".write").click(function () {
+    $(".new-tweet").slideToggle();
+    $("#tweet-text").focus();
+  });
+
+  const scrollFunc = () => {
+    // Get the current scroll value
+    let y = window.scrollY;
+
+    // If the scroll value is greater than the window height, let's add a class to the scroll-to-top button to show it!
+    if (y > 0) {
+      $("#upScroll").show();
+    } else {
+      $("#upScroll").hide();
+    }
+  };
+  $(window).scroll(scrollFunc);
+
+  $("#upScroll").click(function () {
+    $(".new-tweet").slideDown();
+    $("#tweet-text").focus();
+    document.documentElement.scrollTop = 0;
+  });
+
   const $form = $(".submitForm");
   // submit event handler
   $form.on("submit", function (event) {
